79b853b4d37a07fd041e2005b2cd5c5c8058870d
DESKTOP-O8FAA5P\Hongzu <hongzu@ualberta.ca>
2016-10-05 18:17:56 -0600

    add ecgkit

---
+++ Tools/ecg-kit-master/examples/new_example.m
@@ -0,0 +1,16 @@
+clear
+userDir = getuserdir();
+truePath=strcat(userDir,'\ecg-kit-master\*.dat');
+[FileName,PathName] = uigetfile(truePath,'Select a data file');
+filename = fullfile(PathName,FileName); 
+ECGw = ECGwrapper('recording_name',filename);
+plot_ecg_strip(ECGw);
+ECGw.ECGtaskHandle='QRS_detection';
+ECGw.ECGtaskHandle.only_ECG_leads=true;
+% ECGw.ECGtaskHandle.detectors={'wqrs','epltdqrs1'};
+% ECGw.ECGtaskHandle.detectors={'epltdqrs1'};
+ECGw.cacheResults=false;
+tic;ECGw.Run;toc
+delete(filename);
+[pathstr,name,ext]=fileparts(filename);
+delete(strcat(name,'_QRS_detection.mat'));
