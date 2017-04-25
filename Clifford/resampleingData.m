67d69b08c786a9908eb9661f0fa26c1316431cf8
DESKTOP-O8FAA5P\Hongzu <hongzu@ualberta.ca>
2017-04-25 17:21:46 -0600

    the program can now handle both resampling good and bad recordings

--- Clifford/resampleingData.m
+++ Clifford/resampleingData.m
@@ -1,4 +1,13 @@
 function newArray=resampleingData(array,acceptableCount,unacceptableCount,label)
-    Aarray = array(label==1);
-    Uarray = datasample(array(label==-1),acceptableCount,'Replace',true);
+    if(acceptableCount<unacceptableCount)
+        %acceptable record less than unacceptable record
+        Aarray = array(label==1);
+        Uarray = datasample(array(label==-1),acceptableCount,'Replace',true);
+    elseif(acceptableCount>unacceptableCount)
+        %accpetable record more than unacceptable record
+        Aarray = datasample(array(label==1),unacceptableCount,'Replace',true);
+        Uarray = array(label==-1); 
+    else
+        %do nothing
+    end
     newArray=vertcat(Aarray,Uarray);
